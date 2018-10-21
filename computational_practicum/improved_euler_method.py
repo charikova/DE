1c2bb5be1af1c5f8d02f7fc76995b6f4328b7ebb
Mariia Charikova <m.charikova@innopolis.ru>
2018-10-21 17:42:37 +0300

    Add Euler's method function and savings of graphs

---
+++ computational_practicum/improved_euler_method.py
@@ -0,0 +1,33 @@
+from math import *
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import savefig
+
+
+def computations(x0, y0, x, n):
+    xs = []
+    ys = []
+
+    xs.append(x0)
+    ys.append(y0)
+
+    n = int(n)
+
+    h = (x - x0) / n
+
+    for i in range(n):
+        x = xs[-1]
+        y = ys[-1]
+        y_current = (sin(x) * sin(x)) + (y * (cos(x)/sin(x)))
+        x_current = x + h
+        ys.append(y_current)
+        xs.append(x_current)
+
+    plot(xs, ys)
+
+
+def plot(xs, ys):
+    plt.plot(xs, ys, 'o')
+    plt.xlabel('Value of x')
+    plt.ylabel('Value of y')
+    plt.title("Approximate solution with Euler's Method")
+    savefig('improved_euler_method_solution.png')
--- computational_practicum/views.py
+++ computational_practicum/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
+from computational_practicum import euler_method
+from computational_practicum import improved_euler_method
+from computational_practicum import runge_kutta_method
+
 
 def solution(request):
+    parser(request)
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -11,3 +16,17 @@ def solution(request):
     else:
         form = PostForm()
     return render(request, 'computational_practicum/solution.html', {'form': form})
+
+
+def parser(request):
+    q = Post.objects.all()
+    x0 = ([q[0].x0])
+    y0 = ([q[0].y0])
+    x = ([q[0].x])
+    n = ([q[0].n])
+    Post.clean(Post)
+    euler_method.computations(x0[-1], y0[-1], x[-1], n[-1])
+    improved_euler_method.computations(x0[-1], y0[-1], x[-1], n[-1])
+    runge_kutta_method.computations(x0[-1], y0[-1], x[-1], n[-1])
+
+    return render(request, 'computational_practicum/get_solution.html', {'x0': x0, 'y0': y0, 'x': x, 'n': n})
